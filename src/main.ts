cc7d7b69574edeccb3c6bb67648f2a03da09dc5f
bggbr <jaejung603@gmail.com>
2023-06-11 17:46:47 +0900

    add morgan

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import { AppModule } from '@/app/app.module';
 import * as morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.use(morgan('dev'));
-
-  await app.listen(1222);
+    const app = await NestFactory.create(AppModule);
+    app.use(morgan('dev'));
+    await app.listen(1222);
 }
 bootstrap();
